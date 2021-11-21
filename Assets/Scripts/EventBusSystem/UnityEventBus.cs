ceb1e772a53cac4edd104f0609eeaa030c94d137
Bezymyanyj <Bezymyanyj@gmail.com>
2021-11-21 10:40:48 -0500

    Unity event Bus

---
+++ Assets/Scripts/EventBusSystem/UnityEventBus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EventBusSystem.LeopotamGroup.Events;
+using UnityEngine;
+
+namespace EventBusSystem
+{
+    public class UnityEventBus : MonoBehaviour
+    {
+        public EventBus eventBus { get; private set; }
+
+        private void Awake()
+        {
+            eventBus = new EventBus();
+        }
+    }
+}
