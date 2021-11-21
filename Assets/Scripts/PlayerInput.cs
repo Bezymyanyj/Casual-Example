15f37ee0e4c5a3aad232be5a876e897e81cc95ee
Bezymyanyj <Bezymyanyj@gmail.com>
2021-11-21 10:41:17 -0500

    Player Input

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DefaultNamespace
+{
+    public class PlayerInput : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            
+            Debug.Log($"'Start drag {eventData.clickCount}");
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            Debug.Log($" drag");
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            Debug.Log($"End drag");
+        }
+    }
+}
