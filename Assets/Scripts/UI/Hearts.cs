696329df8043c8da98449a4fe4c66b41861f5e7f
Bezymyanyj <Bezymyanyj@gmail.com>
2021-11-21 10:41:58 -0500

    Hearts

---
+++ Assets/Scripts/UI/Hearts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DefaultNamespace.EventHadlers;
+using EventBusSystem;
+using EventBusSystem.LeopotamGroup.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class Hearts : MonoBehaviour
+    {
+        [SerializeField] private UnityEventBus unityEventBus;
+        [SerializeField] private Image[] hearts;
+
+        private int life;
+        
+        private EventBus bus;
+        private void Start()
+        {
+            bus = unityEventBus.eventBus;
+            
+            bus.Subscribe<NegativeAnswer>(OnNegativeAnswer);
+        }
+
+        private void OnDestroy()
+        {
+            bus.Unsubscribe<NegativeAnswer>(OnNegativeAnswer);
+        }
+
+
+        private void OnNegativeAnswer(NegativeAnswer answer)
+        {
+            life--;
+        }
+    }
+}
