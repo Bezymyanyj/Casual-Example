877cfc2a266165c650d674ace73d07d1fba04fcc
Bezymyanyj <Bezymyanyj@gmail.com>
2021-11-21 10:49:48 -0500

    Stars

---
+++ Assets/Scripts/UI/Stars.cs
@@ -0,0 +1,35 @@
+﻿using DefaultNamespace.EventHadlers;
+using EventBusSystem;
+using EventBusSystem.LeopotamGroup.Events;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class Stars : MonoBehaviour
+    {
+        [SerializeField] private UnityEventBus unityEventBus;
+        [SerializeField] private TextMeshProUGUI stars;
+
+        private int starsAmount;
+        
+        private EventBus bus;
+        private void Start()
+        {
+            bus = unityEventBus.eventBus;
+            
+            bus.Subscribe<PositiveAnswer>(OnPositiveAnswer);
+        }
+
+        private void OnDestroy()
+        {
+            bus.Unsubscribe<PositiveAnswer>(OnPositiveAnswer);
+        }
+
+        private void OnPositiveAnswer(PositiveAnswer answer)
+        {
+            starsAmount++;
+            stars.text = starsAmount.ToString();
+        }
+    }
+}
