dcbffb63bdb0f6be5b118831caa4d03460a94a9d
Bezymyanyj <Bezymyanyj@gmail.com>
2021-11-25 09:17:07 -0500

    Handlers

---
+++ Assets/Scripts/EventBusSystem/EventHadlers/LaunchFabricHandler.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace.EventHadlers
+{
+    public class LaunchFabricHandler
+    {
+    }
+}
---
+++ Assets/Scripts/EventBusSystem/EventHadlers/SendNewCardHandle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace.EventHadlers
+{
+    public class SendNewCardHandle
+    {
+        public Card Card;
+        public Vector3 StartPosition;
+        public SendNewCardHandle(Card card, Vector3 position)
+        {
+            Card = card;
+            StartPosition = position;
+        }
+    }
+}
