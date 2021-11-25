616974539dcd566100125b2c37462157cb103fb0
Bezymyanyj <Bezymyanyj@gmail.com>
2021-11-25 09:16:58 -0500

    Card and Fabric

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DefaultNamespace.EventHadlers;
+using EventBusSystem.LeopotamGroup.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class Card : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer cardImage;
+
+
+        private EventBus bus;
+        public void SetImage(Sprite sprite)
+        {
+            cardImage.sprite = sprite;
+        }
+
+        public void SetBus(EventBus bus)
+        {
+            this.bus = bus;
+        }
+
+
+        public void Move(Vector2 position)
+        {
+            transform.localPosition += new Vector3(position.x , 0, 0);
+        }
+
+
+        private void Update()
+        {
+            if (transform.position.x > 3 || transform.position.x < -3)
+            {
+                bus.Publish(new LaunchFabricHandler());
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CardsFabric.cs
@@ -0,0 +1,80 @@
+﻿
+using DefaultNamespace;
+using DefaultNamespace.EventHadlers;
+using EventBusSystem.LeopotamGroup.Events;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using Random = UnityEngine.Random;
+
+namespace EventBusSystem
+{
+    public class CardsFabric : MonoBehaviour
+    {
+        [SerializeField] private AssetReferenceGameObject baseCard;
+        [SerializeField] private AssetReferenceSprite[] cardImages;
+
+        [SerializeField] private UnityEventBus unityEventBus;
+        
+        private EventBus bus;
+        private int imageIndex;
+        private Card newCard;
+        private Transform parent;
+
+        private void Awake()
+        {
+            cardImages = Shuffle(cardImages);
+        }
+
+        private void Start()
+        {
+            bus = unityEventBus.eventBus;
+            
+            bus.Subscribe<LaunchFabricHandler>(OnLaunchFabric);
+            GetNewCard();
+        }
+
+        private void OnLaunchFabric(LaunchFabricHandler data)
+        {
+            GetNewCard();
+        }
+
+        public void GetNewCard()
+        {
+            if (imageIndex >= cardImages.Length)
+            {
+                //eng game
+            }
+            Addressables.LoadAssetAsync<GameObject>(baseCard).Completed += obj =>
+            {
+                if (obj.Status == AsyncOperationStatus.Succeeded)
+                {
+                    var loadedObject = obj.Result;
+                    var instantiatedObject = Instantiate(loadedObject, transform);
+                    newCard = instantiatedObject.GetComponent<Card>();
+                    bus.Publish(new SendNewCardHandle(newCard, newCard.transform.position));
+                    Addressables.LoadAssetAsync<Sprite>(cardImages[imageIndex++]).Completed += handle =>
+                    {
+                        if (handle.Status == AsyncOperationStatus.Succeeded)
+                        {
+                            var loadedSprite = handle.Result;
+                            newCard.SetImage(loadedSprite);
+                            newCard.SetBus(bus);
+                        }
+                    };
+                }
+            };
+        }
+        private AssetReferenceSprite[] Shuffle(AssetReferenceSprite[]  sequence)
+        {
+            var n = sequence.Length;
+            while (n > 1)
+            {
+                n--;
+                var k = Random.Range(0, n + 1);
+                (sequence[k], sequence[n]) = (sequence[n], sequence[k]);
+            }
+            return sequence;
+        }
+    }
+}
