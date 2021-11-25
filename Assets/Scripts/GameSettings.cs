9261b0418f8c911e41d42c4ce31c63669501be4b
Bezymyanyj <Bezymyanyj@gmail.com>
2021-11-25 11:23:52 -0500

    Load setting from csv

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameSettings
+    {
+        public int life;
+        public int timer;
+        public int maxCombo;
+
+        private string settings;
+        public GameSettings(string settings)
+        {
+            this.settings = settings;
+            
+            ReadSettings(settings);
+        }
+
+        private void ReadSettings(string settings)
+        {
+            string[] lines = settings.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            string[] values = new string[lines.Length];
+            
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var columns = lines[i].Split(',').Select(j => j.Trim()).ToList();
+
+               values[i] = columns[1];
+            }
+            int.TryParse(values[1], out life);
+            int.TryParse(values[2], out timer);
+            int.TryParse(values[3], out maxCombo);
+
+            Debug.Log($"Test read csv: {life} {timer} {maxCombo}");
+        }
+    }
+}
---
+++ Assets/Scripts/LoaderSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using EventBusSystem;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace DefaultNamespace
+{
+    public class LoaderSettings : MonoBehaviour
+    {
+        [SerializeField] private AssetReference gameSettings;
+
+        [SerializeField] private UnityEventBus unityEventBus;
+
+        private void Awake()
+        {
+            Addressables.LoadAssetAsync<TextAsset>(gameSettings).Completed += handle =>
+            {
+                if (handle.Status == AsyncOperationStatus.Succeeded)
+                {
+                    var textAsset = handle.Result;
+                    var text = textAsset.text;
+
+                    var a = new GameSettings(text);
+                }
+            };
+        }
+    }
+}
