178b668c9a3a12d03fff5f11c3fb3682d9c7f1a6
Bezymyanyj <Bezymyanyj@gmail.com>
2021-11-25 09:17:50 -0500

    Update PlayerInput.cs

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,24 +1,59 @@
-﻿using UnityEngine;
+﻿using System;
+using DefaultNamespace.EventHadlers;
+using EventBusSystem;
+using EventBusSystem.LeopotamGroup.Events;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace DefaultNamespace
 {
     public class PlayerInput : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
+
+        [SerializeField] private UnityEventBus unityEventBus;
+        
+        private Card card;
+        private Vector3 startPosition;
+        
+        private EventBus bus;
+        private void Start()
+        {
+            bus = unityEventBus.eventBus;
+
+            bus.Subscribe<SendNewCardHandle>(OnNewCardCreated);
+        }
+
         public void OnBeginDrag(PointerEventData eventData)
         {
-            
+            //select card
             Debug.Log($"'Start drag {eventData.clickCount}");
         }
 
         public void OnDrag(PointerEventData eventData)
         {
-            Debug.Log($" drag");
+            Vector3 screenCenter = 
+                new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, -Camera.main.transform.position.z);
+            Vector3 screenTouch = screenCenter + new Vector3(eventData.delta.x, eventData.delta.y, 0);
+            
+            Vector3 worldCenterPosition = Camera.main.ScreenToWorldPoint(screenCenter);
+            Vector3 worldTouchPosition = Camera.main.ScreenToWorldPoint(screenTouch);
+
+            Vector3 delta = worldTouchPosition - worldCenterPosition;
+            card.Move(delta);
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
+            card.transform.position = startPosition;
+            //unselect card
+            //retern to start position
             Debug.Log($"End drag");
+        }
+
+        private void OnNewCardCreated(SendNewCardHandle data)
+        {
+            card = data.Card;
+            startPosition = data.StartPosition;
         }
     }
 }
